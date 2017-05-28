6e419af977dfada705233cf3579b81fb28267dbd
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-05-28 15:02:42 -0300

    beans

---
+++ beans/src/main/java/com/github/kyriosdata/spring/BeansApplication.java
@@ -0,0 +1,44 @@
+package com.github.kyriosdata.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@SpringBootApplication
+public class BeansApplication {
+
+	/**
+	 * Obtenha o valor dessa propriedade por meio do
+	 * bean identificado por "beans".
+	 */
+	@Autowired
+	@Resource(name = "beans")
+	private List<String> beans;
+
+	/**
+	 * Define um bean identificado por "beans".
+	 *
+	 * @return Lista de nomes.
+	 */
+	@Bean
+	public List<String> beans() {
+		return Arrays.asList("primeiro", "segundo");
+	}
+
+	@RequestMapping("/")
+	public String listaConfiguracaoDeBeans() {
+		return String.join(", ", beans);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeansApplication.class, args);
+	}
+}
