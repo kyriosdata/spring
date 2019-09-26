37063bb187a8b5decf67fa2b1e5d049c8820e071
kyriosdata <fabio@inf.ufg.br>
2019-09-26 14:52:11 -0300

    documentacao

--- mongodb/src/main/java/com/github/kyriosdata/spring/mongodb/MongoConfiguracao.java
+++ mongodb/src/main/java/com/github/kyriosdata/spring/mongodb/MongoConfiguracao.java
@@ -1,21 +1,39 @@
 package com.github.kyriosdata.spring.mongodb;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 @Configuration
-public class MongoConfiguracao {
+public class MongoConfiguracao extends AbstractMongoConfiguration {
 
-    @Value("${database.host}")
-    private String mongodbHost;
+    @Value("${mongodb.host}")
+    private String host;
+
+    @Value("${mongodb.port}")
+    private int port;
+
+    @Value("${mongodb.database}")
+    private String database;
 
     @Bean
     public MongoClient mongoClient() {
         System.out.println("--------------");
-        System.out.println("MongoDB host: " + mongodbHost);
+        System.out.println("MongoDB host: " + host + "/" + port);
         System.out.println("--------------");
-        return new MongoClient(mongodbHost);
+        ServerAddress address = new ServerAddress(host, port);
+        MongoClientOptions.Builder builder = MongoClientOptions.builder();
+        builder.connectTimeout(1000);
+        MongoClient mc = new MongoClient(address, builder.build());
+        return mc;
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return database;
     }
 }
