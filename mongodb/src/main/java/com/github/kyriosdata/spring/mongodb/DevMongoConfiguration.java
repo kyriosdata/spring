e2bc14dd7c7b1235afa99f76a1736f986d98abd7
kyriosdata <fabio@inf.ufg.br>
2019-09-26 15:57:46 -0300

    padrao

--- mongodb/src/main/java/com/github/kyriosdata/spring/mongodb/DevMongoConfiguration.java
+++ mongodb/src/main/java/com/github/kyriosdata/spring/mongodb/DevMongoConfiguration.java
@@ -1,10 +1,6 @@
 package com.github.kyriosdata.spring.mongodb;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
