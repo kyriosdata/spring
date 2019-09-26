acec35dbb90f07fee83d20aa729a019052519f6c
kyriosdata <fabio@inf.ufg.br>
2019-09-26 15:57:11 -0300

    documentacao

--- mongodb/src/main/java/com/github/kyriosdata/spring/mongodb/DevMongoConfiguration.java
+++ mongodb/src/main/java/com/github/kyriosdata/spring/mongodb/DevMongoConfiguration.java
@@ -10,26 +10,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 /**
- * Obtém valores do arquivo <em>application.properties</em>, em particular,
- * <em>mongodb.host</em>, <em>mongodb.port</em> e <em>mongodb.database</em>.
- * Estas propriedades ainda podem ser fornecidas por meio de argumentos ao
- * programa (por exemplo, --mongodb.port=89) e variáveis de ambiente.
+ * Cria instância para a cesso ao MongoDB em localhost:27017 (valores padrão).
  */
 @Configuration
 @Profile("dev")
 public class DevMongoConfiguration {
 
-    @Value("${mongodb.host}")
-    private String host;
-
-    @Value("${mongodb.port}")
-    private int port;
-
     @Bean
     public MongoClient mongoClient() {
-        MongoClientOptions options = MongoClientOptions.builder().build();
-        ServerAddress address = new ServerAddress(host, port);
-
-        return new MongoClient(address, options);
+        return new MongoClient();
     }
 }
