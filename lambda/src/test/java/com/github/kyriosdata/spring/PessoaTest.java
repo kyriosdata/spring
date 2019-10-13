232dc19cc1faffe04840f54c8f9504b7c64aef18
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-10-13 18:58:05 -0300

    mais testes

--- lambda/src/test/java/com/github/kyriosdata/spring/PessoaTest.java
+++ lambda/src/test/java/com/github/kyriosdata/spring/PessoaTest.java
@@ -31,4 +31,30 @@ class PessoaTest {
 		assertTrue(idosaComNomeRaro.test(pessoa));
 	}
 
+	@Test
+	void lambdaExemplo() {
+		Predicate<Pessoa> jovem = p -> p.getIdade() < 18;
+		Predicate<Pessoa> noPeso = p -> p.getPeso() < 90;
+		Predicate<Pessoa> nomeBonito = p -> p.getNome().equals("Davi");
+
+		// Interessante é a pessoa jovem com menos de 90 quilos e nome bonito,
+		// ou, se estas condições não forem satisfeitas,
+		// se for pessoa "alta", mais de 1.9 de altura.
+		Predicate<Pessoa> interessante = jovem
+				.and(nomeBonito.and(noPeso))
+				.or(p -> p.getAltura() > 1.9);
+
+		Pessoa davi = new Pessoa();
+		davi.setNome("Davi");
+		davi.setIdade(17);
+		davi.setPeso(89);
+
+		assertTrue(interessante.test(davi));
+
+		// Reutilizando mesma instância
+		// Pessoa não é um "value object", não é immutable.
+		davi.setIdade(20);
+		davi.setAltura(2);
+		assertTrue(interessante.test(davi));
+	}
 }
