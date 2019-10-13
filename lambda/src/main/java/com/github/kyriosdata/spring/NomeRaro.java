3e472978ac773b998172e4150f4ea273206b4b1c
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-10-13 18:44:58 -0300

    testes basicos para ilustrar lambdas

--- lambda/src/main/java/com/github/kyriosdata/spring/NomeRaro.java
+++ lambda/src/main/java/com/github/kyriosdata/spring/NomeRaro.java
@@ -3,7 +3,7 @@ package com.github.kyriosdata.spring;
 import java.util.function.Predicate;
 
 public class NomeRaro implements Predicate<Pessoa> {
-    
+
     @Override
     public boolean test(Pessoa pessoa) {
         return pessoa.getNome().length() < 6;
--- lambda/src/test/java/com/github/kyriosdata/spring/PessoaTest.java
+++ lambda/src/test/java/com/github/kyriosdata/spring/PessoaTest.java
@@ -2,10 +2,33 @@ package com.github.kyriosdata.spring;
 
 import org.junit.jupiter.api.Test;
 
-public class PessoaTest {
+import java.util.function.Predicate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PessoaTest {
 
 	@Test
-	public void contextLoads() {
+	void pessoaComNomeRaro() {
+
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Davi");
+
+		assertTrue(new NomeRaro().test(pessoa));
+	}
+
+	@Test
+	void pessoaIdosaComNomeRaro() {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setIdade(61);
+		pessoa.setNome("Maria");
+
+		assertTrue(new NomeRaro().and(new Idoso()).test(pessoa));
+
+		Predicate<Pessoa> nomeRaro = new NomeRaro();
+		Predicate<Pessoa> idosa = new Idoso();
+		Predicate<Pessoa> idosaComNomeRaro = idosa.and(nomeRaro);
+		assertTrue(idosaComNomeRaro.test(pessoa));
 	}
 
 }
